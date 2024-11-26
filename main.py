104362b114f966f5cff6ebb394d958e4dd958f3e
xNeverAlone <barretsv@gmail.com>
2024-11-26 21:09:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+first = int(input("Vvedite chislo: "))
+second = int(input("Vvedite chislo: "))
+third = int(input("Vvedite chislo: "))
+if first != second != third:
+    print(0)
+if first == second and third:
+    print(3)
+if first == second or third:
+    print(2)
